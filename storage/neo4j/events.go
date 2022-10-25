15bc6c9a5197d14160c691dbd35d4e57cc44d315
Jakob Høgenes <1014990+jakhog@users.noreply.github.com>
2022-10-25 10:14:09 +0200

    All things stored in Neo4j

--- storage/neo4j/events.go
+++ storage/neo4j/events.go
@@ -9,6 +9,7 @@ import (
 	"context"
 	"dolittle.io/fleet-observer/entities"
 	"github.com/neo4j/neo4j-go-driver/v5/neo4j"
+	"time"
 )
 
 type Events struct {
@@ -24,13 +25,86 @@ func NewEvents(session neo4j.SessionWithContext, ctx context.Context) *Events {
 }
 
 func (e *Events) Set(event entities.Event) error {
-	return nil
+	return runMultiUpdate(
+		e.session,
+		e.ctx,
+		map[string]any{
+			"uid":               event.UID,
+			"count":             event.Properties.Count,
+			"firstTime":         event.Properties.FirstTime.Format(time.RFC3339),
+			"lastTime":          event.Properties.LastTime.Format(time.RFC3339),
+			"platform":          event.Properties.Platform,
+			"link_instance_uid": event.Links.HappenedToDeploymentInstanceUID,
+		},
+		`
+			MERGE (event:`+event.Type+`:Event { _uid: $uid })
+			SET event = { _uid: $uid, count: $count, firstTime: $firstTime, lastTime: $lastTime, platform: $platform }
+			RETURN id(event)
+		`,
+		`
+			MATCH (event:Event { _uid: $uid })
+			WITH event
+				MERGE (instance:DeploymentInstance { _uid: $link_instance_uid })
+				WITH event, instance
+					MERGE (event)-[:HappenedTo]->(instance)
+					WITH event, instance
+						MATCH (event)-[r:HappenedTo]->(other)
+						WHERE other._uid <> instance._uid
+						DELETE r
+			RETURN id(event)
+		`)
 }
 
 func (e *Events) Get(id entities.EventUID) (*entities.Event, bool, error) {
-	return nil, false, nil
+	event := &entities.Event{}
+	found, err := findSingleJson(
+		e.session,
+		e.ctx,
+		map[string]any{
+			"uid": id,
+		},
+		`
+			MATCH (event:Event { _uid: $uid })-[:HappenedTo]->(instance:DeploymentInstance)
+			WITH {
+				uid: event._uid,
+				type: apoc.coll.removeAll(labels(event), ["Event"])[0],
+				properties: {
+					count: event.count,
+					firstTime: toString(event.firstTime),
+					lastTime: toString(event.lastTime),
+					platform: event.platform
+				},
+				links: {
+					happenedTo: instance._uid
+				}
+			} as entry
+			RETURN apoc.convert.toJson(entry) as json
+		`,
+		event)
+	return event, found, err
 }
 
 func (e *Events) List() ([]entities.Event, error) {
-	return nil, nil
+	var events []entities.Event
+	return events, querySingleJson(
+		e.session,
+		e.ctx,
+		`
+			MATCH (event:Event)-[:HappenedTo]->(instance:DeploymentInstance)
+			WITH {
+				uid: event._uid,
+				type: apoc.coll.removeAll(labels(event), ["Event"])[0],
+				properties: {
+					count: event.count,
+					firstTime: toString(event.firstTime),
+					lastTime: toString(event.lastTime),
+					platform: event.platform
+				},
+				links: {
+					happenedTo: instance._uid
+				}
+			} as entry
+			RETURN apoc.convert.toJson(collect(entry)) as json
+		`,
+		&events)
 }
--- storage/neo4j/utils.go
+++ storage/neo4j/utils.go
@@ -13,6 +13,7 @@ import (
 )
 
 var (
+	ErrFoundMoreThanOneRecord         = errors.New("found more than one record while looking for a single one")
 	ErrResultRecordDoesNotContainJson = errors.New("the resulting record did not contain a field called 'json'")
 	ErrResultJsonFieldWasNotString    = errors.New("the field called 'json' in the resulting record was not a string")
 )
@@ -48,6 +49,30 @@ func runMultiUpdate(session neo4j.SessionWithContext, ctx context.Context, param
 	return err
 }
 
+func findSingleJson(session neo4j.SessionWithContext, ctx context.Context, params map[string]any, cypher string, v any) (bool, error) {
+	result, err := session.Run(ctx, cypher, params)
+	if err != nil {
+		return false, err
+	}
+
+	if !result.Next(ctx) {
+		return false, nil
+	}
+
+	record := result.Record()
+
+	foundMore := false
+	for result.Next(ctx) {
+		foundMore = true
+	}
+
+	if foundMore {
+		return false, ErrFoundMoreThanOneRecord
+	}
+
+	return true, decodeRecordAsJson(record, v)
+}
+
 func querySingleJson(session neo4j.SessionWithContext, ctx context.Context, cypher string, v any) error {
 	result, err := session.Run(ctx, cypher, nil)
 	if err != nil {
@@ -59,6 +84,10 @@ func querySingleJson(session neo4j.SessionWithContext, ctx context.Context, cyph
 		return err
 	}
 
+	return decodeRecordAsJson(record, v)
+}
+
+func decodeRecordAsJson(record *neo4j.Record, v any) error {
 	data, exists := record.Get("json")
 	if !exists {
 		return ErrResultRecordDoesNotContainJson
